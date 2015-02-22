db0d2693cf1194780c1df7fa7984ab0ddecafebf
Chris Loew <cloew123@gmail.com>
2015-02-22 03:18:46 -0700

    Smart Defaults can now be set to copy the default for each invocation

--- src/kao_decorators/smart_defaults.py
+++ src/kao_decorators/smart_defaults.py
@@ -1,3 +1,4 @@
+import copy
 import inspect
 
 def get_default_args(func):
@@ -11,9 +12,10 @@ def get_default_args(func):
 class Default:
     """ Represents a default argument """
     
-    def __init__(self, argument):
+    def __init__(self, argument, perInstance=False):
         """ Initialize the default """
         self.argument = argument
+        self.perInstance = perInstance
         
     def shouldUseDefault(self, kwargs):
         """ Return if the default value should be used """
@@ -26,11 +28,22 @@ class Default:
     def getDefault(self, defaults):
         """ Return get the default value for the argument """
         return defaults[self.argument]
+        
+class PerCallDefault(Default):
+    """ Represents a default that should be copied for each execution of the function """
+    
+    def shouldUseDefault(self, kwargs):
+        """ Return if the default value should be used """
+        return Default.shouldUseDefault(self, kwargs) or self.argument not in kwargs
+        
+    def getDefault(self, defaults):
+        """ Return get the default value for the argument """
+        return copy.deepcopy(defaults[self.argument])
 
 def smart_defaults(*args):
     """ Set the given arguments to be used as smart properties that 
         can be set to None and then set to the actual default value """
-    defaults = [Default(arg) for arg in args]
+    defaults = [Default(arg) if type(arg) is str else arg for arg in args]
     def getFnDefaults(fn):
         defaultArgs = get_default_args(fn)
         def setKwargs(*args, **kwargs):
