818cc8c007d76f20312e4b0438527755df71c07f
Chris Loew <cloew123@gmail.com>
2015-02-22 03:19:25 -0700

    Version bumb and author details

--- src/setup.py
+++ src/setup.py
@@ -1,9 +1,9 @@
 from distutils.core import setup
 
 setup(name='kao_decorators',
-      version='0.0.1',
-      description="",
-      author='',
-      author_email='',
+      version='0.0.2',
+      description="Collection of useful decorators",
+      author='Chris Loew',
+      author_email='cloew123@gmail.com',
       packages=['kao_decorators'],
      )
