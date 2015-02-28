c1940f8765f7c428451ec3c204e2a4f1da99bb93
Chris Loew <cloew123@gmail.com>
2015-02-27 17:21:56 -0700

    Updated the smart defaults so it works when keyword args are provided inline

--- src/kao_decorators/smart_defaults.py
+++ src/kao_decorators/smart_defaults.py
@@ -1,55 +1,91 @@
 import copy
 import inspect
 
-def get_default_args(func):
-    """
-    returns a dictionary of arg_name:default_values for the input function
-    """
-    args, varargs, keywords, defaults = inspect.getargspec(func)
-    return dict(zip(reversed(args), reversed(defaults)))
+class FunctionMetadata:
+    """ Represents a functions metadata """
     
+    def __init__(self, func):
+        """ Initialize the metadata """
+        self.args, varargs, keywords, defaults = inspect.getargspec(func)
+        self.defaults = dict(zip(reversed(self.args), reversed(defaults)))
+        
+    def getArgIndex(self, arg):
+        """ Return the inline argument index for a given argument """
+        return self.args.index(arg)
+        
+class SmartArg:
+    """ Helper class to handle interacting with a particular argument """
     
+    def __init__(self, argName, funcMetadata):
+        """ Initialize the smart argument with its name and the metadata of the function """
+        self.argName = argName
+        self.inlineIndex = funcMetadata.getArgIndex(self.argName)
+        
+    def isProvided(self, args, kwargs):
+        """ Return if this argument has been specified in the given arguments """
+        return self.argName in kwargs or len(args) > self.inlineIndex
+        
+    def getValue(self, args, kwargs):
+        """ Return the argument value """
+        if len(args) > self.inlineIndex:
+            return args[self.inlineIndex]
+        else:
+            return kwargs[self.argName]
+        
+    def setValue(self, args, kwargs, newValue):
+        """ Set the argument to the specified value """
+        if len(args) > self.inlineIndex:
+            args[self.inlineIndex] = newValue
+        else:
+            kwargs[self.argName] = newValue
+            
 class Default:
     """ Represents a default argument """
     
-    def __init__(self, argument, perInstance=False):
+    def __init__(self, argument):
         """ Initialize the default """
-        self.argument = argument
-        self.perInstance = perInstance
+        self.argName = argument
         
-    def shouldUseDefault(self, kwargs):
+    def setMetadata(self, metadata):
+        """ Set the default's metadata """
+        self.argument = SmartArg(self.argName, metadata)
+        self.metadata = metadata
+        
+    def shouldUseDefault(self, args, kwargs):
         """ Return if the default value should be used """
-        return self.argument in kwargs and kwargs[self.argument] is None
+        return self.argument.isProvided(args, kwargs) and self.argument.getValue(args, kwargs) is None
         
-    def setDefault(self, kwargs, defaults):
+    def setDefault(self, args, kwargs):
         """ Return get the default value for the argument """
-        kwargs[self.argument] = self.getDefault(defaults)
+        self.argument.setValue(args, kwargs, self.getDefault())
         
-    def getDefault(self, defaults):
+    def getDefault(self):
         """ Return get the default value for the argument """
-        return defaults[self.argument]
+        return self.metadata.defaults[self.argument.argName]
         
 class PerCallDefault(Default):
     """ Represents a default that should be copied for each execution of the function """
     
-    def shouldUseDefault(self, kwargs):
+    def shouldUseDefault(self, args, kwargs):
         """ Return if the default value should be used """
-        return Default.shouldUseDefault(self, kwargs) or self.argument not in kwargs
+        return Default.shouldUseDefault(self, args, kwargs) or not self.argument.isProvided(args, kwargs)
         
-    def getDefault(self, defaults):
+    def getDefault(self):
         """ Return get the default value for the argument """
-        return copy.deepcopy(defaults[self.argument])
+        return copy.deepcopy(Default.getDefault(self))
 
 def smart_defaults(*args):
     """ Set the given arguments to be used as smart properties that 
         can be set to None and then set to the actual default value """
     defaults = [Default(arg) if type(arg) is str else arg for arg in args]
     def getFnDefaults(fn):
-        defaultArgs = get_default_args(fn)
+        metadata = FunctionMetadata(fn)
+        [default.setMetadata(metadata) for default in defaults]
         def setKwargs(*args, **kwargs):
+            args = list(args)
             for default in defaults:
-                if default.shouldUseDefault(kwargs):
-                    default.setDefault(kwargs, defaultArgs)
+                if default.shouldUseDefault(args, kwargs):
+                    default.setDefault(args, kwargs)
             return fn(*args, **kwargs)
         return setKwargs
     return getFnDefaults
--- src/setup.py
+++ src/setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='kao_decorators',
-      version='0.0.2',
+      version='0.0.3',
       description="Collection of useful decorators",
       author='Chris Loew',
       author_email='cloew123@gmail.com',
